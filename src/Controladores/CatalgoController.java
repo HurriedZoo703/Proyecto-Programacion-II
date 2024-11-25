de7ab075527deda253cfe7b228a66f43f6764bee
Ivan Anaya <ivan.manuelanaya3@gmail.com>
2024-11-25 07:40:14 -0500

    Agregando metodo para cerrar sesión en el controlador del catalogo.

--- src/Controladores/CatalgoController.java
+++ src/Controladores/CatalgoController.java
@@ -1,14 +1,44 @@
 package Controladores;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class CatalgoController implements Initializable {
+
+    @FXML
+    private Text btn_cerrarS;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
     
+    public void closeWindow() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Vistas/Login.fxml"));
+
+            Parent roott = loader.load();
+
+            Scene scene = new Scene(roott);
+            Stage stage = new Stage();
+
+            stage.setScene(scene);
+            stage.show();
+
+            Stage miStage = (Stage) this.btn_cerrarS.getScene().getWindow();
+            miStage.close();
+        } catch (IOException ex) {
+            Logger.getLogger(CatalgoController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
