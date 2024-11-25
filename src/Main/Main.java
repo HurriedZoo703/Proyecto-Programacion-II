cf379675ba6185285f027a018eb766cfee2ce0dd
Ivan Anaya <ivan.manuelanaya3@gmail.com>
2024-11-25 03:24:50 -0500

    Creando fxml para la vista del catalogo.

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         try {
-            Parent root = FXMLLoader.load(getClass().getResource(""));
+            Parent root = FXMLLoader.load(getClass().getResource("/Vistas/Login.fxml"));
             Scene scene = new Scene(root);
             stage.setScene(scene);
             stage.show();
