a6952831529b39a63e6661792ba3b69e4dc82ad5
Ivan Anaya <ivan.manuelanaya3@gmail.com>
2024-11-28 01:14:18 -0500

    Modificación del nodo camiseta.

--- src/Modelos/NodoCamiseta.java
+++ src/Modelos/NodoCamiseta.java
@@ -2,18 +2,30 @@ package Modelos;
 
 public class NodoCamiseta {
     
+    private int idPropietario;
     private String URL_ima;
     private String talla;
     private String color;
     private float precio;
+    private int tComprado;
 
-    public NodoCamiseta(String URL_ima, String talla, String color, float precio) {
+    public NodoCamiseta(int idPropietario, String URL_ima, String talla, String color, float precio) {
+        this.idPropietario = idPropietario;
         this.URL_ima = URL_ima;
         this.talla = talla;
         this.color = color;
         this.precio = precio;
+        this.tComprado = 0;
     }
 
+    public int getIdPropietario() {
+        return idPropietario;
+    }
+
+    public void setIdPropietario(int idPropietario) {
+        this.idPropietario = idPropietario;
+    }
+    
     public String getURL_ima() {
         return URL_ima;
     }
@@ -45,5 +57,13 @@ public class NodoCamiseta {
     public void setPrecio(float precio) {
         this.precio = precio;
     }       
-    
+
+    public int gettComprado() {
+        return tComprado;
+    }
+
+    public void settComprado(int tComprado) {
+        this.tComprado = tComprado;
+    }
+       
 }
