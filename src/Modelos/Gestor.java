a89159a5fbd892f6b293c6f9eaf4af29378d0272
Ivan Anaya <ivan.manuelanaya3@gmail.com>
2024-11-28 01:40:44 -0500

    Agregando pilas al gestor.

--- src/Modelos/Gestor.java
+++ src/Modelos/Gestor.java
@@ -4,9 +4,11 @@ public class Gestor {
 
     private static Gestor instancia;
     private final ListaUsuarios lista;
-
+    private final PilaCamisetas pilaCamiseta;
+    
     private Gestor() {
         this.lista = new ListaUsuarios();
+        this.pilaCamiseta = new PilaCamisetas();
     }
 
     public static Gestor obtenerInstancia() {
@@ -21,5 +23,9 @@ public class Gestor {
     public ListaUsuarios getLista() {
         return lista;
     }
-    
+
+    public PilaCamisetas getPilaCamiseta() {
+        return pilaCamiseta;
+    }
+        
 }
